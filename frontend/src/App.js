2e035f5287f6f872b2cea8e6734986d32cad80b0
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2020-01-16 15:42:55 +0700

    :sparkles: Add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
-// import Flashcard from './Flashcard';
+import Flashcard from './Flashcard';
 import CardList from './CardList';
 
 function App() {
   return (
-    <div>
-      {/* <Flashcard /> */}
-      <CardList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Memorize</Link>
+            </li>
+            <li>
+              <Link to="/crud">CRUD</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/crud">
+            <CardList />
+          </Route>
+          <Route path="/">
+            <Flashcard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Flashcard.js
+++ frontend/src/Flashcard.js
@@ -3,19 +3,19 @@ import './Flashcard.scss';
 
 function Flashcard() {
   return (
-    <div>
-      <article className="flashcard">
-        <input id="flashcard-1" type="checkbox" />
-        <label htmlFor="flashcard-1">
-          <section className="front">Front of the flashcard!</section>
-
-          <section className="back">Back of the flashcard!</section>
-        </label>
-      </article>
-      <button type="button">Previous</button>
-      <button type="button">Next</button>
-      <button type="button">Random</button>
-      <button type="button">Flip</button>
+    <div className="container">
+      <div
+        className="flip-container"
+        ontouchstart="this.classList.toggle('hover');"
+      >
+        <div className="flippable appcon ac-bicycle">
+          <div className="front">
+            <span>B</span>
+            <i className="fa fa-bicycle" />
+          </div>
+          <div className="back">Bicycle</div>
+        </div>
+      </div>
     </div>
   );
 }
