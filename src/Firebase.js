c8cb9eaef0f5c34c8f9fc92e788cad77c0b3217d
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2020-01-18 16:34:29 +0700

    :star: Update firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,11 +4,11 @@ import 'firebase/firestore';
 const settings = { timestampsInSnapshots: true };
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: ''
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 };
 firebase.initializeApp(config);
 
