0e25681f5bc89296d786d05ce7662336494a3fd7
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2020-02-08 18:47:04 +0700

    :star: Pagination UI

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -106,6 +106,37 @@ class CardList extends Component {
             </tr>
           </tbody>
         </table>
+        <nav>
+          <ul className="pagination pagination-lg justify-content-center">
+            <li className="page-item">
+              <a className="page-link" href="!#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span className="sr-only">Previous</span>
+              </a>
+            </li>
+            <li className="page-item active">
+              <a className="page-link" href="!#">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="!#">
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="!#">
+                3
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="!#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span className="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
         <div className="crude-form__wrapper">
           <h3>Edit</h3>
           <h3>Add</h3>
