1553e1700f8224a39e320451442a1e62e669ae92
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2020-02-17 21:13:13 +0700

    :star: Heroku

--- webpack.common.js
+++ webpack.common.js
@@ -22,7 +22,16 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        HELLO: JSON.stringify('Define variable from webpack')
+        HELLO: JSON.stringify('Define variable from webpack'),
+        FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+        FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        FIREBASE_DATABASE_URL: JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        FIREBASE_STORAGE_BUCKET: JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        )
       }
     }),
     // https://webpack.js.org/plugins/progress-plugin/
